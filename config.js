42836aaa16a268a0372db4bf7a2e7e7bfd01ce33
tandong156 <85281435+tandong156@users.noreply.github.com>
2021-06-03 15:55:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 const CONFIG = {
-    introTitle: 'Hey Bae Đần ❤️',
-    introDesc: `Đây là một thứ táo làm trong lúc rảnh rỗi sinh nông nỗi...để mà `,
-    btnIntro: 'để mà làm gì còn lâu mới nói ><',
-    title: 'Đùa chứ...chỉ là rủ ng yêu đi ăn thôi à',
-    desc: 'Chủ nhật đi ăn nhó ❤️❤️❤️',
-    btnYes: 'Vâng ạ',
-    btnNo: 'Không, Mơ đi ',
-    question:'Ăn gì thì liệt kê trước để anh còn tìm quán nè',
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://m.me/dongkoi2806' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
